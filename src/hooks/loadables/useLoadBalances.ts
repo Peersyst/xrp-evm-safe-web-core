3d949735e74327945a0d61cdcb861ac7a5c8336f
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-12-22 17:34:26 +0100

    Add token price workaround

--- src/hooks/loadables/useLoadBalances.ts
+++ src/hooks/loadables/useLoadBalances.ts
@@ -5,6 +5,7 @@ import useAsync, { type AsyncResult } from '../useAsync'
 import { Errors, logError } from '@/services/exceptions'
 import { selectCurrency } from '@/store/settingsSlice'
 import { selectSafeInfo } from '@/store/safeInfoSlice'
+import { BigNumber, ethers } from 'ethers'
 
 export const useLoadBalances = (): AsyncResult<SafeBalanceResponse> => {
   // use the selector directly because useSafeInfo is memoized
@@ -15,7 +16,27 @@ export const useLoadBalances = (): AsyncResult<SafeBalanceResponse> => {
   const [data, error, loading] = useAsync<SafeBalanceResponse | undefined>(
     async () => {
       if (!safe) return
-      return getBalances(safe.chainId, safe.address.value, currency)
+
+      const res: any = await fetch('https://api.coingecko.com/api/v3/coins/ripple')
+      const data = await res.json()
+      const balances = await getBalances(safe.chainId, safe.address.value, currency)
+      return balances.items.reduce(
+        (res, { balance, tokenInfo }) => {
+          const fiatConversion = (data?.market_data?.current_price['usd'] as number).toString()
+          const fiatBalanceBN = Number(ethers.utils.formatUnits(balance)) * Number(fiatConversion)
+          return {
+            fiatTotal: (Number(res.fiatTotal) + Number(fiatBalanceBN)).toString(),
+            items: [
+              ...res.items,
+              { balance, fiatConversion: fiatConversion, fiatBalance: fiatBalanceBN.toString(), tokenInfo },
+            ],
+          }
+        },
+        {
+          fiatTotal: '0',
+          items: [],
+        } as SafeBalanceResponse,
+      )
     },
     [safe, currency], // Reload either when the Safe is updated or the currency changes
     false, // Don't clear data between SafeInfo polls
